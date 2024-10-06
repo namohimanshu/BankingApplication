fb722cc2b1c8afd8378c93e870cd1b6aff042074
Himanshu Singh <himanshusingh6612@gmail.com>
2024-10-06 16:12:11 +0530

    created model class

---
+++ src/main/java/com/himanshu/bankApplication/Model/Address.java
@@ -0,0 +1,5 @@
+package com.himanshu.bankApplication.Model;
+
+public class Address {
+
+}
---
+++ src/main/java/com/himanshu/bankApplication/Model/Customer.java
@@ -0,0 +1,50 @@
+package com.himanshu.bankApplication.Model;
+
+public class Customer {
+	private String name;
+	private String gender;
+    private final long Account_num ;
+	private Address address;
+	public Customer(String name, String gender, long account_num, Address address) {
+		super();
+		this.name = name;
+		this.gender = gender;
+		Account_num = account_num;
+		this.address = address;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	public long getAccount_num() {
+		return Account_num;
+	}
+	
+	public long setAccount_num(long acc_num) {
+		this.
+	}
+	@Override
+	public String toString() {
+		return "Customer [name=" + name + ", gender=" + gender + ", Account_num=" + Account_num + ", address=" + address
+				+ "]";
+	}
+	
+	
+	
+	
+
+}
