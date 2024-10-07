cc7b9d00f45f3c0d4892e58842e2f17e82cf9e9e
himanshuk10 <cusat.himanshu@gmail.com>
2024-10-07 20:09:24 +0530

    Added get by id method

--- src/main/java/com/himanshu/bankApplication/Service/CustomerService.java
+++ src/main/java/com/himanshu/bankApplication/Service/CustomerService.java
@@ -1,6 +1,7 @@
 package com.himanshu.bankApplication.Service;
 
 import com.himanshu.bankApplication.Model.Customer;
+import jakarta.persistence.Id;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,5 +12,6 @@ public interface CustomerService {
     public List<Customer> getAllCustomer();
     public  Customer addCustomer(Customer customer);
     public Customer getCustomerByAccountNum (long Account_num);
+    public Customer getCustomerById(Long id);
 
 }
